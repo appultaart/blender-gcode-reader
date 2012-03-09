e3d3a15b30b461a63bb02d408d9dc70b3c360b02
Winter Guerra <mr.servergeek@gmail.com>
2012-03-09 06:17:49 -0500

    The shell script now has partial functionality. It does not log though...

--- blenderRunWithDebug.sh
+++ blenderRunWithDebug.sh
@@ -6,12 +6,23 @@
 # Release under the Creative Commons CC_BY_SA license
 # March 1st, 2012
 
-blenderInstallLocation=/Applications/Blender_2.62_x64/blender.app/Contents/MacOS
+SearchDirectory="/Applications"
 
-echo Opening blender at $blendersInstallLocation
+#Can we find the blender install location by regexing /Applications ? Why not?
+blenderInstallLocation=$(ls -p $SearchDirectory | grep -i 'blender.*2\.6.*/')
+
+#Check if there is more than 1 possible install location and throw a warning if so
+if [ $(wc -l $blenderInstallLocation) > "1" ]; then
+echo "Arg! More than one install location found in $SearchDirectory !"
+exit 1
+fi
+
+#TODO: Ask the user to choose which is the installation folder and save it in a config file.
+
+echo "Opening blender executable at $SearchDirectory/$blenderInstallLocation"
 
 #Show work
-echo $blenderInstallLocation/./blender
+echo "$SearchDirectory/${blenderInstallLocation}blender.app/Contents/MacOS/./blender"
 
 #Execute Blender
-$blenderInstallLocation/./blender
+$SearchDirectory/${blenderInstallLocation}blender.app/Contents/MacOS/./blender
