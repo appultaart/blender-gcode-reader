5c9cb53b8be9a5dd0e49fbf3056fe56a6399c63c
Douwe van der Veen <appultart-github@yahoo.com>
2012-04-21 22:46:52 +0200

    Include the functionality to import .gcode file and to show the curves inside Blender, without the need to do so by loading the 'Blender_gcode.py' file into the Blender Script Editor.
    With this __init__.py file, now a user is able to select the addon
    from the user preferences (Addon >> Import/Export/). When activated,
    both a file menu entry appears as well as a panel (where a check
    can be placed to use a bevel object or not). When the file is loaded,
    a blender GcurveData object is drawn onto the screen.
    
    (This is similar as running the Blender_gcode.py file from a script)
    
    This code works with the latest revision as of April 21, see version info
    for the exact blender revision nr.

--- __init__.py
+++ __init__.py
@@ -2,43 +2,13 @@
 # __init__.py #
 ###############
 
-# To support reload properly, try to access a package var, 
-# if it's there, reload everything
-if "bpy" in locals():
-    import imp
-    imp.reload(blenderGcode)
-    imp.reload(parseGcode)
-    print("Reloaded Blender_import_gcode, Gcode_parser")
-else:
-    from . import Blender_import_gcode as blenderGcode
-    from . import Gcode_parser as parseGcode
-    print("Imported Blender_import_gcode, Gcode_parser")
-
-import bpy 
-
-from bpy.props import StringProperty
-
-
-bl_info = {
-    'name': 'Import GCode',
-    'author': 'Simon Kirkby',
-    'version': (0,0,7),
-    'blender': (2, 6, 2),
-    'api': 45241,
-    'location': 'File > Import-Export > Gcode',
-    'description': 'Import and visualize gcode files generated for 3D printers (.gcode)',
-    "wiki_url": "",
-    "tracker_url": "",
-    'category': 'Import-Export'}
-
-__version__ = '.'.join([str(s) for s in bl_info['version']])
-
+"""
 # gcode parser for blender 2.5 
 # Simon Kirkby
 # 201102051305
 # tigger@interthingy.com 
 
-#modified by David Anderson to handle Skeinforge comments
+# modified by David Anderson to handle Skeinforge comments
 # Thanks Simon!!!
 
 # modified by Alessandro Ranellucci (2011-10-14)
@@ -59,85 +29,134 @@ __version__ = '.'.join([str(s) for s in bl_info['version']])
 # part from the Blender drawing part: converted to multi-files addon.
 # Thanks to Jelle Boomstra for suggestions, advice, help.
 
+"""
 
-class IMPORT_OT_gcode(bpy.types.Operator):
-    '''Imports gcode used by 3D printers such as Reprap, Makerbot, Ultimaker'''
-    bl_idname = "import_scene.gcode"
-    bl_description = 'Gcode reader, reads tool moves and animates layers build'
-    bl_label = "Import gcode" +' v.'+ __version__
-    bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW"
 
-    filepath = StringProperty(name="File Path", 
-                            description="Filepath used for importing the GCode file", 
-                            maxlen= 1024, 
-                            default= "")
+# To support reload properly, try to access a package var, 
+# if it's there, reload everything
+if "bpy" in locals():
+    import imp
+    imp.reload(blenderGcode)
+    imp.reload(parseGcode)
+    print("Reloaded Blender_import_gcode, Gcode_parser")
+else:
+    from . import Blender_import_gcode as blenderGcode
+    from . import Gcode_parser as parseGcode
+    print("Imported Blender_import_gcode, Gcode_parser")
 
-    ##### DRAW #####
-    def draw(self, context):
-        layout0 = self.layout
-         
-    def execute(self, context):
-#        global toggle, theMergeLimit, theCodec, theCircleRes
+import bpy 
 
-        # do the things needed to build a file
+from bpy_extras.io_utils import ImportHelper
+from bpy.props import StringProperty, BoolProperty
+
+
+bl_info = {
+    'name': 'DOUWE Import GCode',
+    'author': 'Douwe tests this',       #'Simon Kirkby',
+    'version': (0,0,7),
+    'blender': (2, 6, 2),
+    'api': 45822,
+    'location': 'File > Import-Export > Gcode',
+    'description': 'Import and visualize gcode files generated for 3D printers (.gcode)',
+    'category': 'Import-Export'}
+
+__version__ = '.'.join([str(s) for s in bl_info['version']])
+
+
+# ---------------------------------------------
+class IMPORT_OT_gcode(bpy.types.Operator, ImportHelper):
+    """Class to open the File Selector to select a .gcode file
+    """
+    bl_idname= "import_scene.import_gcode"
+    bl_description = 'Use the File Selector to import a .gcode file'
+    bl_label = "Import GcodeZ"
+    filename_ext = ".gcode"
+    filter_glob = StringProperty(default="*.gcode", options={'HIDDEN'})    
+    filepath= StringProperty(name="File Path", description="Filepath used for importing the .gcode file", maxlen=1024, default="")
+    
+    def execute(self, context): 
+## ADD FUNCTIONAL STUFF HERE
+        # Initiate a virtual 3D printing machine
         myMachine = parseGcode.Machine()
         print("OK: initiated Machine:", myMachine)
 
+        # Import the .gcode file and add as an extruder
         myMachine.add_extruder(self.filepath)
         print("OK: add extruder to current machine.")
 
-        myGcodes = myMachine.extruders[-1].standardGcode
-        print("OK: Gcode commands are fetched from the current extruder.")
+        # select the extruder we'd like to draw
+        extruderToDraw = myMachine.extruders[-1]
 
+        # Get the extruder we'd like to draw, and transfer that data to a gcodeCurvesData object
+        myGcodeCurvesData = blenderGcode.gcodeCurvesData(extruderToDraw)
 
-    # The gcodeData object is a dictionary that stores gcodeCurveData objects.
-    # Each gcodeCurveData object has the name of the 'Z' value, and in this 
-    # object all the commands with that 'Z' value are stored.
-        mygcodeCurveDatas = blenderGcode.sort_commands_to_layers(myGcodes)                    # input is list, returns dict
-        layerNames = sorted(blenderGcode.gcodeCurveData._registry)
-        print("Layer names are:", layerNames)
+        # Sort the Gcode commands by Z-value, and create 'gcodeCurve' instance for every Z value
+        myGcodeCurvesData.add_gcode_to_gcodeCurves()
 
-    # For every layer (i.e., every gcodeCurveData object), walk through the 
-    # commands and convert to splines. For Blender, every layer (= Z-value) 
-    # stores one BezierCurve object. This curve can have multiple splines.
-        for layer in mygcodeCurveDatas.values():
-            layer.create_splines()
-            print(layer)
+        # Obtain a sorted list of all the names of the layers, i.e., all Z values.
+        Z_layerNames = sorted(blenderGcode.gcodeCurve._registry)
 
-    # There might well be layers with commands but without splines. We will
-    # remove these, as they will clutter the code (NB such spline-less layers
-    # are normally the result of a repositioning command like G92)
+        # Create splines data
+        for Zval in Z_layerNames:
+            myGcodeCurvesData.gcodeCurves[Zval].create_splines_data()
+
+        # There might well be layers with commands but without splines. We will
+        # remove these, as they will clutter the code (NB such spline-less layers
+        # are normally the result of a repositioning command like G92)
         noSplineInLayer = list()
-        for layer in layerNames:
-            if mygcodeCurveDatas[layer].count_splines() == 0:                           # no splines are present
-                noSplineInLayer.append(mygcodeCurveDatas.pop(layer))                    # pop the layer with 0 splines
-                blenderGcode.gcodeCurveData._registry.remove(layer)                                  # remove Z-value from registry
+        for layer in Z_layerNames:
+            if myGcodeCurvesData.gcodeCurves[layer].count_splines() == 0:                           # no splines are present
+                noSplineInLayer.append(myGcodeCurvesData.gcodeCurves.pop(layer))                    # pop the layer with 0 splines
+                blenderGcode.gcodeCurve._registry.remove(layer)
         print("These layers were removed as they contain no splines:", noSplineInLayer)
-
-        # Hack to reselect the layernames: layers with 0 curves are removed, so we need to update
-        layerNames = sorted(blenderGcode.gcodeCurveData._registry)
+    
+        # Hack to reselect the Z_layerNames: layers with 0 curves are removed, so we need to update
+        Z_layerNames = sorted(blenderGcode.gcodeCurve._registry)
     
         # let's start drawing in Blender!
         # First, make a bevel object in case we'd like to use one
-        bev = blenderGcode.make_bevel_object()                                                   # make a bevel object
-        
-        # Draw the curves to our scene
-        blenderGcode.draw_blenderBeziers(mygcodeCurveDatas)                                          # no bevel object
-    #    draw_blenderBeziers(mygcodeCurveDatas, bev)                                    # with bevel object    
-    
-        # animate the curves
-        blenderGcode.animate_blenderBeziers(layerNames)
-    
-#### 
+        myGcodeCurvesData.create_bevel_object()
 
+        myGcodeCurvesData.draw_blender_bezier_curves(use_bevel = bpy.context.scene.use_bevel)
+ 
+ ## END FUNCTIONAL STUFF
         return {'FINISHED'}
 
+
     def invoke(self, context, event):
-        wm = context.window_manager
-        wm.fileselect_add(self)
+        context.window_manager.fileselect_add(self)
         return {'RUNNING_MODAL'}
 
+
+class OBJECT_OT_CloseGcodePanelButton(bpy.types.Operator):
+    """Class for a button that can close the .gcode import panel
+    """
+    bl_idname = "import_scene.close_panel"
+    bl_label = "Close this Gcode panel"
+    
+    def execute(self, context):
+        print("Now in the ClosePanelButton execute function...")
+        bpy.types.INFO_MT_file_import.remove(menu_func)
+        bpy.utils.unregister_module(__name__)
+        return {'FINISHED'}
+
+
+class GcodeToolPropertiesPanel(bpy.types.Panel):
+    """Class that creates a .gcode import panel inside the Tool Properties panel.
+    """
+    bl_label = "Panel: Import .gcode"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOL_PROPS"
+
+    def draw(self, context):
+        self.layout.operator("import_scene.import_gcode", text='Import a .gcode file')
+        self.layout.prop(context.scene, "use_bevel") 
+        self.layout.operator("import_scene.close_panel", text = 'Close this panel')
+# ----------------------------        
+
+bpy.types.Scene.use_bevel = BoolProperty(name = "Use Bevel", 
+                                         description = "Whether or not to use a bevel object",
+                                         default = False)
 
 
 
@@ -148,6 +167,7 @@ def register():
     bpy.utils.register_module(__name__)
     bpy.types.INFO_MT_file_import.append(menu_func)
  
+
 def unregister():
     bpy.utils.unregister_module(__name__)
     bpy.types.INFO_MT_file_import.remove(menu_func)
